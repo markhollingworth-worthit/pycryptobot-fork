ea90db250763752e5219a46f8a4630f851215368
markhollingworth-worthit <mhollingworth@gmail.com>
2022-05-04 16:05:18 +0100

    Revert "move import statement"
    This reverts commit 93f1ab99f7db225434c3b26d445758522d5d67f7.

--- pycryptobot.py
+++ pycryptobot.py
@@ -31,7 +31,7 @@ from models.Stats import Stats
 from models.TradingAccount import TradingAccount
 from views.TradingGraphs import TradingGraphs
 from os.path import exists as file_exists
-# from models.Strategy import Strategy
+from models.Strategy import Strategy
 
 app = PyCryptoBot()
 account = TradingAccount(app)
@@ -58,7 +58,7 @@ else:
 
 
 # from views.TradingGraphs import TradingGraphs
-from models.Strategy import Strategy
+
 # minimal traceback
 sys.tracebacklimit = 1
 
